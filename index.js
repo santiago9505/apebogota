4f678de4e02a454de527cdfa5330d61cfb5febc2
santiago9505 <zagroller@hotmail.com>
2020-08-04 06:55:03 -0500

    Cambios de accesibilidad

--- index.js
+++ index.js
@@ -12,4 +12,24 @@ function enlace()
 {
   location.href = "https://forms.office.com/Pages/ResponsePage.aspx?id=gcPCyy4vk02R0VBskxas5_uaYzbSud1LmU6-S958qnFUMVZEQ0paT0JLVzFGOE5JNEJYVzNEWDQyUS4u"
 }
- 
+
+
+var audio = document.getElementById("audio-accesible")
+
+var teclas = {
+  ESC: 27,
+  C: 67,
+  F: 70
+};
+
+document.addEventListener("keyup", reproducirAudio);
+function reproducirAudio(evento) 
+{
+  if(evento.keyCode == teclas.ESC)
+  {
+    audio.play();  
+  }
+  else {
+    audio.pause(audio);
+  }
+}
