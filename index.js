f219b89cb2897735ea53b227cba2ad9f769e0f3d
santiago9505 <zagroller@hotmail.com>
2020-08-05 15:00:52 -0500

    Agrego audio de formación

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ function enlace()
   location.href = "https://forms.office.com/Pages/ResponsePage.aspx?id=gcPCyy4vk02R0VBskxas5_uaYzbSud1LmU6-S958qnFUMVZEQ0paT0JLVzFGOE5JNEJYVzNEWDQyUS4u"
 }
 
-var audio1 = document.getElementById("audio1-accesible")
+var audio1 = document.getElementById("audio1-accesible");
 var audio2 = document.getElementById("audio2-accesible");
+var audio3 = document.getElementById("audio3-accesible");
 
 var teclas = {
   ESC: 27,
   C: 67,
   SPACE: 32,
   CTRL: 17,
-  F: 70
+  BORRAR: 8,
+  F: 70,
 };
 
 document.addEventListener("keyup", reproducirAudio);
@@ -36,11 +38,15 @@ function reproducirAudio(evento)
     audio1.pause();
     audio2.play();  
   }
-  else{
-    audio2.pause();
-  }
   if(evento.keyCode == teclas.CTRL)
   {
     audio1.pause();
+    audio2.pause();
+    audio3.pause();
+  }
+  if(evento.keyCode == teclas.BORRAR)
+  {
+    audio1.pause();
+    audio3.play();
   }
 }
