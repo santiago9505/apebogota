0e6704730cbc9e4e16963a60c504ea4996020130
santiago9505 <zagroller@hotmail.com>
2020-08-04 23:38:25 -0500

    Agrego javascript a los audios

--- index.js
+++ index.js
@@ -13,23 +13,34 @@ function enlace()
   location.href = "https://forms.office.com/Pages/ResponsePage.aspx?id=gcPCyy4vk02R0VBskxas5_uaYzbSud1LmU6-S958qnFUMVZEQ0paT0JLVzFGOE5JNEJYVzNEWDQyUS4u"
 }
 
-
-var audio = document.getElementById("audio-accesible")
+var audio1 = document.getElementById("audio1-accesible")
+var audio2 = document.getElementById("audio2-accesible");
 
 var teclas = {
   ESC: 27,
   C: 67,
+  SPACE: 32,
+  CTRL: 17,
   F: 70
 };
 
 document.addEventListener("keyup", reproducirAudio);
 function reproducirAudio(evento) 
 {
+  if(evento.keyCode == teclas.SPACE)
+  {
+    audio1.play();
+  }
   if(evento.keyCode == teclas.ESC)
   {
-    audio.play();  
+    audio1.pause();
+    audio2.play();  
   }
-  else {
-    audio.pause(audio);
+  else{
+    audio2.pause();
+  }
+  if(evento.keyCode == teclas.CTRL)
+  {
+    audio1.pause();
   }
 }
