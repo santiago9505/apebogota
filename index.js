4ca63234cb4952ee93d14fe472c2e8116824b878
Santiago Guáqueta <zagroller@hotmail.com>
2020-10-01 21:17:09 -0500

    Cambio funciones de accesibilidad

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function enlace()
 var audio1 = document.getElementById("audio1-accesible");
 var audio2 = document.getElementById("audio2-accesible");
 var audio3 = document.getElementById("audio3-accesible");
+var audio4 = document.getElementById("audio-emprendimiento");
 
 var teclas = {
   ESC: 27,
@@ -23,36 +24,50 @@ var teclas = {
   SPACE: 32,
   CTRL: 17,
   BORRAR: 8,
+  E: 69,
   F: 70
 };
 
 document.addEventListener("keyup", reproducirAudio);
 function reproducirAudio(evento) 
 {
+  
   if(evento.keyCode == teclas.SPACE)
   {
     audio1.play();
     audio2.pause();
     audio3.pause();
-  }
-  if(evento.keyCode == teclas.ESC)
-  {
-    audio1.pause();
-    audio3.pause();
-    audio2.play();  
-  }
-  if(evento.keyCode == teclas.CTRL)
-  {
-    audio1.pause();
-    audio2.pause();
-    audio3.pause();
+    audio4.pause();
   }
   if(evento.keyCode == teclas.BORRAR)
   {
     audio1.pause();
     audio2.pause();
     audio3.play();
+    audio4.pause();
   }
+  if(evento.keyCode == teclas.E)
+  {
+    audio1.pause();
+    audio2.pause();
+    audio3.pause();
+    audio4.play();
+  }
+  if(evento.keyCode == teclas.ESC)
+  {
+    audio1.pause();
+    audio3.pause();
+    audio2.play();  
+    audio4.pause();
+  }
+  if(evento.keyCode == teclas.CTRL)
+  {
+    audio1.pause();
+    audio2.pause();
+    audio3.pause();
+    audio4.pause();
+  }
+ 
   if(evento.keyCode == teclas.C)
   {
     enlace();
