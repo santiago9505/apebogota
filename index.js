4f2d829da346763986c9e5b5d71afd29f05206af
Santiago Guáqueta <zagroller@hotmail.com>
2020-10-03 22:36:23 -0500

    Creando turnos

--- index.js
+++ index.js
@@ -8,15 +8,74 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+
+
+
 function enlace()
 {
   location.href = "https://forms.office.com/Pages/ResponsePage.aspx?id=gcPCyy4vk02R0VBskxas5_uaYzbSud1LmU6-S958qnFUMVZEQ0paT0JLVzFGOE5JNEJYVzNEWDQyUS4u"
+}
+
+var correo = document.getElementById('email');
+var password = document.getElementById('password');
+var error = document.getElementById('error');
+error.style.color = 'red';
+
+
+
+function enviarFormulario(){
+  console.log("enviando formulario");
+
+  
+
+  var mensajesError = [];
+
+  if(correo.value === "sguaqueta@sena.edu.co" && password.value ==='Sena2020*'){
+    mensajesError.push('Ingresa tu contraseña');
+
+    var turnos = [];
+    const funcionarios = document.getElementById('funcionarios')
+    const parrafo = document.getElementById('atencion1');
+    const modulo = parrafo.textContent;
+    const ancla = document.getElementById('salelink');
+
+    parrafo.addEventListener('click', tuLink);
+      function tuLink() {
+        let link = prompt('link de tu reunión');
+        parrafo.textContent = "Turno: ";
+        ancla.textContent = link;
+        ancla.href = link;
+      }
+    
+
+   
+  }
+
+  if(correo.value === null || correo.value ===''){
+    mensajesError.push('Ingresa tu correo');
+  }
+
+
+  if(password.value === null || password.value ===''){
+    mensajesError.push('Ingresa tu contraseña');
+  }
+
+  error.innerHTML = mensajesError.join(', ');
+
+  return false;
 }
 
 var audio1 = document.getElementById("audio1-accesible");
 var audio2 = document.getElementById("audio2-accesible");
 var audio3 = document.getElementById("audio3-accesible");
 var audio4 = document.getElementById("audio-emprendimiento");
+
+
+
+
+  
+
+
 
 var teclas = {
   ESC: 27,
@@ -73,3 +132,5 @@ function reproducirAudio(evento)
     enlace();
   }
 }
+
+
